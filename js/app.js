cabfd660319aa7a0d558fca33bc61dc3462833c6
My name <me@example.com>
2020-06-30 11:00:42 +0530

    Changes add for mobile displays

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 const navSlide =() =>{
     const burger = document.querySelector(".burger");
     const nav =document.querySelector(".nav-links");
+    const links = document.querySelectorAll('.nav-links li a');
     
     burger.addEventListener('click',() => {
     nav.classList.toggle('toggle');
     burger.classList.toggle('cross');
-    })};
+    })
+    links[0].addEventListener('click',()=>{
+        nav.classList.toggle('toggle');
+    burger.classList.toggle('cross');
+    });
+    links[1].addEventListener('click',()=>{
+        nav.classList.toggle('toggle');
+    burger.classList.toggle('cross');
+    });
+    links[2].addEventListener('click',()=>{
+        nav.classList.toggle('toggle');
+    burger.classList.toggle('cross');
+    });
+};
+    
     
 navSlide();
     
